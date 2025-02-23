3170582e233289e11de467203d817a55db2e98c6
Benjamin Schulze <benjamsc@paul-f4Br3s6.clusters.42paris.fr>
2025-02-24 00:08:03 +0100

    norm check

--- srcs/ft_reverse_bits.c
+++ srcs/ft_reverse_bits.c
@@ -6,7 +6,7 @@
 /*   By: moa <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 05:37:04 by moa               #+#    #+#             */
-/*   Updated: 2025/02/04 05:57:51 by moa              ###   ########.fr       */
+/*   Updated: 2025/02/24 00:07:12 by benjamsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,20 @@ unsigned char	ft_reverse_bits(unsigned char octet)
 	return (tmp);
 }
 
-unsigned int ft_reverse_octets(unsigned int octets)
+unsigned int	ft_reverse_octets(unsigned int octets)
 {
-    unsigned int result;
-	unsigned char current;
-	unsigned char reversed;
-    int i;
+	unsigned int	result;
+	unsigned char	current;
+	unsigned char	reversed;
+	int				i;
 
 	result = 0;
 	i = -1;
-    while (++i < 4)
-    {
-        current = (octets >> (8 * i)) & 0xFF;
-        reversed = ft_reverse_bits(current);
-        result |= (unsigned int)reversed << (8 * (3 - i));
-    }
-    return (result);
+	while (++i < 4)
+	{
+		current = (octets >> (8 * i)) & 0xFF;
+		reversed = ft_reverse_bits(current);
+		result |= (unsigned int)reversed << (8 * (3 - i));
+	}
+	return (result);
 }
-/**/
-/*#include <stdio.h>*/
-/**/
-/*int	main(int argc, char **argv)*/
-/*{*/
-/*	int	nb;*/
-/**/
-/*	nb = ft_atoi(argv[1]);*/
-/*	printf("swap :%d", ft_reverse_bits(nb));*/
-/*}*/
