5340daa46c56f3a7c9d9303c72d8e67f51b2c590
Benjamin Schulze <benjamsc@paul-f4Ar6s7.clusters.42paris.fr>
2025-03-29 03:20:08 +0100

    MAJ

--- includes/ft_def.h
+++ includes/ft_def.h
@@ -16,13 +16,20 @@
 # include "libft.h"
 
 # define RESET "\033[0m"
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define YELLOW "\033[0;33m"
-# define BLUE "\033[0;34m"
-# define MAGENTA "\033[0;35m"
-# define CYAN "\033[0;36m"
-# define WHITE "\033[0;37m"
+# define RED "\033[0;31m" //V
+# define GREEN "\033[0;32m" //V
+# define YELLOW "\033[0;33m" //V
+# define BLUE "\033[0;34m" //V
+# define MAGENTA "\033[0;35m" //V
+# define CYAN "\033[0;36m" //V
+# define WHITE "\033[0;37m" //V
+# define ORANGE "\033[0;91m" //V
+# define PURPLE "\033[0;95m"
+# define DARK_ORANGE "\033[0;33m"
+# define PINK "\033[0;95m"
+# define TEAL "\033[0;96m"
+# define GRAY "\033[0;90m"
+
 
 typedef struct s_list
 {
