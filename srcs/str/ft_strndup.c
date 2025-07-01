80bcdec01f5fd44c364874852437b8e5c39e77a4
Moac <benjamsc@student.42.fr>
2025-07-01 16:41:44 +0200

    lil changes strndup

--- srcs/str/ft_strndup.c
+++ srcs/str/ft_strndup.c
@@ -3,20 +3,19 @@
 
 char	*ft_strndup(const char *s, t_uint n)
 {
-	int		l_src;
 	char	*str;
 	char	*src;
 
 	if (!s)
 		return (NULL);
-	l_src = ft_strlen(s);
-	str = (char *)ft_calloc((l_src + 1), sizeof(char));
+	str = (char *)ft_calloc((n + 1), sizeof(char));
 	if (! str)
 		return (0);
 	src = str;
-	while (*s || n > 0)
+	while (*s && n > 0)
 	{
 		*(str++) = *(s++);
 		n--;
 	}
 	return (src);
+}
